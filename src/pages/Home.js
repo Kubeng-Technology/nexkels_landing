78e09a9330377251d77efee2752bb81a7bf1055d
papikelscode <nwakanmakelechi8@gmail.com>
2023-05-11 12:04:32 +0100

    landingpage

---
+++ src/pages/Home.js
@@ -0,0 +1,151 @@
+import logo from '../assets/images/team.jpg'
+import team from '../assets/videos/team.mp4'
+
+
+export default function Home(){
+    return(
+        <div className="Home">
+           <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Services</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>Contact</a></li>
+       
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost normal-case text-xl">nexkels</a>
+  </div>
+  <div className="navbar-end">
+    <button className="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+    <button className="btn btn-ghost btn-circle">
+      <div className="indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+        <span className="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+  </div>
+</div>
+<div className="hero h-full bg-base-200 ">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <video controls width="1000rem" height="100" className="team max-w-sm rounded-lg shadow-2xl">
+        <source   src={team} width="500rem" height="30rem" />
+    </video>
+    <div>
+      <h1 className="text-5xl font-bold py-4 w-5/3" >Leading <br></br> <span className="text-blue-600"> Software Engineers</span> <br></br> To Scale Up Your Tech Business</h1>
+      <p className="para py-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+      <button className="btn btn-primary">Learn More</button>
+    </div>
+
+  </div>
+</div>
+<div className="hero min-h-screen bg-base-200">
+ 
+    <div className="about">
+    <div className='column'>
+        <h3>OUR CORE VALUE</h3>
+        <h1>We Take Care Of <br></br>  <span>
+        Your Digital Problems
+            </span> </h1>
+
+            <div className="split flex flex-col w-full lg:flex-row">
+  <div className="dance grid flex-grow h-40 card rounded-box place-items-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  </div> 
+  <div className="divider lg:divider-horizontal"></div> 
+  <div className="dance grid flex-grow h-40 card  rounded-box place-items-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  </div></div>
+    </div>
+    <div className='columns'>
+    <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  Consultation Service To Achieve Big Goal
+  </div>
+  <div className="collapse-content">
+    <p className='col-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+  </div>
+</div>
+<div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  Training To Make Sure Well Deliver To End User
+  </div>
+  <div className="collapse-content">
+    <p className='col-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+  </div>
+</div>
+<div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  Controlling Accompaniment While We Go!
+  </div>
+  <div className="collapse-content">
+    <p className='col-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+  </div>
+</div>
+    </div>
+</div>
+
+    </div>
+    <div className="hero min-h-screen bg-base-200" id='hero2'>
+ 
+    <div className="about">
+    <div className='column'>
+        <h3>WHY CHOOSE US</h3>
+        <h1>We provide <br></br>  <span>
+        Perfect IT Solutions
+            </span> <br></br>
+            For Your Business </h1>
+
+            <div className="split flex flex-col w-full lg:flex-row">
+  <div className="dance grid flex-grow h-40 card rounded-box place-items-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  </div> 
+  <div className="divider lg:divider-horizontal"></div> 
+  <div className="dance grid flex-grow h-40 card  rounded-box place-items-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+  </div></div>
+    </div>
+    <div className='columns'>
+    <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  Consultation Service To Achieve Big Goal
+  </div>
+  <div className="collapse-content">
+    <p className='col-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+  </div>
+</div>
+<div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  Training To Make Sure Well Deliver To End User
+  </div>
+  <div className="collapse-content">
+    <p className='col-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+  </div>
+</div>
+<div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  Controlling Accompaniment While We Go!
+  </div>
+  <div className="collapse-content">
+    <p className='col-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+  </div>
+</div>
+    </div>
+</div>
+
+    </div>
+ 
+  </div>
+
+    )
+}
